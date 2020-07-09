4acdc96efb31de4e34d73aeda855de3e0bbc75a8
@itsAj <ajaymati1000@gmail.com>
2020-07-10 03:18:29 +0530

    pwa added

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+var CACHE_NAME= 'version-2'
+var urlsToCache = ['/','/index.html','/styles.css','/favicon.ico'];
+self.addEventListener('install',(event)=>{
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache)=>{
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+})
+
+self.addEventListener('fetch',function (event) {
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+        )
+})
+
+self.addEventListener('activate',function (event) {
+    var cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then((cacheNames)=>
+        Promise.all(cacheNames.map((cacheName)=>{
+           if(cacheWhitelist.indexOf(cacheName) === -1)
+            return caches.delete(cacheName);
+        }))
+        )
+    )
+    })
